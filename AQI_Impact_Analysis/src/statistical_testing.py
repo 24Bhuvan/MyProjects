dacdb9c92648d41918ee3394f2196b05b431c8be
24Bhuvan <ubhuvan533@gmail.com>
2025-03-12 20:46:48 +0530

    Shriked statistic tests for ease of understanding

--- AQI_Impact_Analysis/src/statistical_testing.py
+++ AQI_Impact_Analysis/src/statistical_testing.py
@@ -1,13 +1,10 @@
 from scipy import stats
 import pandas as pd
 
-def perform_statistical_test(data_path='synthetic_data.csv'):
+def perform_statistical_tests(data_path='synthetic_data.csv'):
     data = pd.read_csv(data_path)
     high_aqi = data[data['AQI'] > 100]['Engagement Count']
     low_aqi = data[data['AQI'] <= 100]['Engagement Count']
     
     t_stat, p_val = stats.ttest_ind(high_aqi, low_aqi, equal_var=False)
     print(f"T-Statistic: {t_stat}, P-Value: {p_val}")
-    
-if __name__ == '__main__':
-    perform_statistical_test()
